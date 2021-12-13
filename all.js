8b7e49c12ddd3e33e1f269ac2245c7e6e66b9f8d
Spm-Eng <dlspm0512@gmail.com>
2021-12-13 22:41:29 +0800

    feat: new gif

--- all.js
+++ all.js
@@ -1,9 +1,25 @@
 const decodeImage = document.getElementById('decode-image');
+const christmasImage = document.getElementById('christmas-image');
 const wrapper = document.getElementById('wrapper');
 
 (function(){
   let url = location.search.split("?")[1];
+  browserWidth = document.body.clientWidth;
   
+  if (browserWidth > 1400) {
+    imageWidth = Math.ceil(browserWidth * 0.5);
+  } else if (browserWidth > 767) {
+    imageWidth = Math.ceil(browserWidth * 0.6);
+  } else if (browserWidth > 414) {
+    imageWidth = Math.ceil(browserWidth * 1);
+  } else {
+    imageWidth = Math.ceil(browserWidth * 1);
+  }
+  
+  console.log(browserWidth, imageWidth);
+
+  christmasImage.style.width = imageWidth + 'px';
+
   if (url === "decode-text=hello5566") {
     decodeImage.classList.toggle('show');
     wrapper.style.display = 'none';
