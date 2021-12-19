c12c9d6f23aaf24a8273f91cd581cd4baa94f0ca
plin045 <95859509+plin045@users.noreply.github.com>
2021-12-19 22:08:44 +0800

    1219

--- all.js
+++ all.js
@@ -24,7 +24,7 @@ const wrapper = document.getElementById('wrapper');
 
   christmasImage.style.width = imageWidth + 'px';
 
-  if (url === "decode-text=hello5566") {
+  if (url === "decode-text=Fattys Owner") {
     decodeImage.classList.toggle('show');
     wrapper.style.display = 'none';
   }
